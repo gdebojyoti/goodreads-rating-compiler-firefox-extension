7e1b25a47e44ec7b84bf1be537e0d83f411b790b
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-31 18:35:05 +0530

    Redundant code removed.

--- popup/marvelWiki.js
+++ popup/marvelWiki.js
@@ -1,9 +1,8 @@
 (async function () {
   const commonModuleUrl = chrome.runtime.getURL('../modules/utils.js')
-  const { waitForElementToLoad, isDate } = await import(commonModuleUrl)
+  const { waitForElementToLoad } = await import(commonModuleUrl)
 
   const selector = '#mw-content-text table.wikitable'
-  // const tables = document.querySelector('#mw-content-text .wikitable')
 
   await waitForElementToLoad(selector)
 
@@ -15,11 +14,6 @@
   const result = []
 
   tables.forEach((tableElm, index) => {
-    // // ignore the first table
-    // if (!index) {
-    //   return
-    // }
-
     const rows = tableElm.querySelectorAll('tr')
     rows.forEach(elm => {
       // check if elm is valid - i.e., if children is exactly 7
@@ -37,31 +31,6 @@
       const text = titleElm.innerText.replace(/[\t\r\n]/g, '');
       console.log(text)
 
-      // check for volumes
-      if (text.length <= 2) {
-        // get previous entry
-        const previousEntry = result[result.length - 1]
-
-        // set its vol to 1 if it doesn't exist
-        if (typeof previousEntry.volume === "undefined") {
-          previousEntry.volume = 1
-        }
-
-        // add new entry
-        result.push({
-          title: previousEntry.title,
-          volume: parseInt(text)
-        })
-
-        // exit current interation of loop
-        return
-      }
-
-      // ignore date values
-      if (isDate(text)) {
-        return
-      }
-      
       result.push({
         title: text
       })
